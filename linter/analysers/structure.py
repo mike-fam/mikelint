4801fd276dc4b17131d96bedfa38ef3089308e2d
Mike <mikepham1207@gmail.com>
2021-04-26 18:43:07 +1000

    Line length checker

--- linter/analysers/structure.py
+++ linter/analysers/structure.py
@@ -1,5 +1,17 @@
-from linter.analysers.analyser import Analyser
+from linter.analysers.analyser import Analyser, register_check
 
 
 class StructureAnalyser(Analyser):
-    pass
+    @register_check("Number of lines longer than {0} characters: {1}\n"
+                    "\tLines {2} are longer than {0} characters")
+    def check_line_length(self):
+        long_lines_80 = [line_num + 1 for line_num, line in self._source
+                         if len(line) > 80]
+        long_lines_100 = [line_num + 1 for line_num, line in self._source
+                          if len(line) > 100]
+        long_lines_120 = [line_num + 1 for line_num, line in self._source
+                          if len(line) > 120]
+        return [(80, len(long_lines_80), ", ".join(long_lines_80)),
+                (100, len(long_lines_80), ", ".join(long_lines_100)),
+                (120, len(long_lines_80), ", ".join(long_lines_120)),
+                ]
