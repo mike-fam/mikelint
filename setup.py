377bb85c5c77bd56b97fe69944bc98d56e80256c
Mike <mikepham1207@gmail.com>
2021-06-04 13:57:49 +1000

    Setup and config for 2021s1a3

---
+++ setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="mikelint",
+    version="1.0.0",
+    description="Linter used for CSSE1001 at UQ",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mike-fam/mikelint",
+    author="Mike Pham",
+    author_email="mikepham1207@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["reader"],
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "realpython=reader.__main__:main",
+        ]
+    },
+)
